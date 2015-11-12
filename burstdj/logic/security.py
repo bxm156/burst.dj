f14f6417fdce15fe2f8811e0d5af75eee4c00c2c
Jeryl Contemprato <jcontemp@yelp.com>
2015-11-12 15:38:01 -0800

    add login/logout support.  add skeleton room activity api

---
+++ burstdj/logic/security.py
@@ -0,0 +1,36 @@
+from pyramid.security import Allow, Authenticated
+from pyramid.security import Everyone
+
+
+"""username to password"""
+USERS = {
+    'jcontemp': '',
+    'bmarty': '',
+    'ptiet': '',
+    'markon': '',
+    'rroeder': '',
+}
+
+"""username to list of groups"""
+GROUPS = {
+    'jcontemp': [
+        'group:admin'
+    ],
+}
+
+
+def groupfinder(userid, request):
+    """This is where we'd verify the user exists.  return None if not."""
+    if userid in USERS:
+        return GROUPS.get(userid, [])
+
+
+class RootFactory(object):
+    """match groups to permissions"""
+    __acl__ = [
+        (Allow, Everyone, 'view'),
+        (Allow, Authenticated, 'authenticated'),
+        (Allow, 'group:admin', 'admin'),
+    ]
+    def __init__(self, request):
+        pass
