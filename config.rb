290e59100d3bbfe09973b470a686fc0641a2b9fe
Bryan Marty <bmarty@yelp.com>
2015-11-12 12:56:39 -0800

    SASSY

--- config.rb
+++ config.rb
@@ -3,11 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "burstdj/static/css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
