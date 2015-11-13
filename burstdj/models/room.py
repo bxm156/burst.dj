6d5ef484ee11181e5604f5945ab84f1d1cea6b62
MarkoNerandzic <markonerandzic@yahoo.com>
2015-11-12 16:39:52 -0800

    Added RoomUser And RoomQueue models

--- burstdj/models/room.py
+++ burstdj/models/room.py
@@ -1,11 +1,12 @@
 from burstdj.models import *
+from burstdj.models.user import User
 
 class Room(Base):
     __tablename__ = 'room'
     id = Column(Integer, primary_key=True)
     time_created = Column(DateTime)
     name = Column(String)
-    current_user_id = Column(Integer, ForeignKey("user.id"))
+    current_user_id = Column(Integer, ForeignKey(User.id))
     #TODO: Make a ForeignKey when we get track models implemented
     current_track_id = Column(Integer)
     time_track_started = Column(DateTime)
