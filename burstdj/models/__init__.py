f5feabb9f39d645382ba538abca9e2b7c47077a0
Robert Roeder <rroeder@YELP-RROEDER.local>
2015-11-12 16:14:06 -0800

    sqlalchemy relations

--- burstdj/models/__init__.py
+++ burstdj/models/__init__.py
@@ -10,6 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relation,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
